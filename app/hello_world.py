f08abc1c864aeaba51fdbfeb8a202976819689a7
Federico Baroni <fbaroni@gmail.com>
2023-04-18 16:15:46 +0000

    added hello_world, requirements and Dockerfile

---
+++ app/hello_world.py
@@ -0,0 +1,8 @@
+# Import the Streamlit library
+import streamlit as st
+
+# Create a title for your app
+st.title("Hello, World!")
+
+# Create a text output
+st.write("Welcome to my first Streamlit app!")
